cf6e2415fff7db88167b685946ec0f1df9723f42
Nick Pfundstein <nickpfundstein@gmail.com>
2022-12-02 09:09:08 -0700

    Day 2 Part 2 complete

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -3,6 +3,7 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
@@ -23,6 +24,31 @@ fn part1() {
             ("X", "C") | ("Y", "A") | ("Z", "B") => score += 6,
             ("X", "A") | ("Y", "B") | ("Z", "C") => score += 3,
             ("X", "B") | ("Y", "C") | ("Z", "A") => score += 0,
+            _ => unreachable!(),
+        }
+    }
+
+    println!("{score}");
+}
+
+fn part2() {
+    let file = BufReader::new(File::open("input.txt").unwrap());
+
+    let mut score = 0;
+    for line in file.lines().map(|l| l.unwrap()) {
+        let (them, me) = line.split_once(' ').unwrap();
+
+        match me {
+            "X" => score += 0,
+            "Y" => score += 3,
+            "Z" => score += 6,
+            _ => unreachable!(),
+        }
+
+        match (me, them) {
+            ("X", "B") | ("Y", "A") | ("Z", "C") => score += 1,
+            ("X", "C") | ("Y", "B") | ("Z", "A") => score += 2,
+            ("X", "A") | ("Y", "C") | ("Z", "B") => score += 3,
             _ => unreachable!(),
         }
     }
