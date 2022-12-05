edc0ffcad285b0295ab4405ba6fc185862d49c2a
Nick Pfundstein <nickpfundstein@gmail.com>
2022-12-05 09:28:11 -0700

    Day 3 Part 2 complete

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -4,6 +4,7 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
@@ -17,6 +18,32 @@ fn part1() {
         let r_chars = right.chars().collect::<HashSet<_>>();
 
         for &char in l_chars.intersection(&r_chars) {
+            let priority = if char.is_ascii_lowercase() {
+                char as u32 - 96
+            } else {
+                char as u32 - 64 + 26
+            };
+
+            sum += priority
+        }
+    }
+
+    println!("{sum}");
+}
+
+fn part2() {
+    let file = BufReader::new(File::open("input.txt").unwrap());
+
+    let lines = file.lines().map(|l| l.unwrap()).collect::<Vec<_>>();
+    let mut sum = 0;
+    for chunk in lines.chunks(3) {
+        let one = chunk[0].chars().collect::<HashSet<_>>();
+        let two = chunk[1].chars().collect::<HashSet<_>>();
+        let three = chunk[2].chars().collect::<HashSet<_>>();
+
+        let first = one.intersection(&two).copied().collect::<HashSet<_>>();
+
+        for &char in first.intersection(&three) {
             let priority = if char.is_ascii_lowercase() {
                 char as u32 - 96
             } else {
